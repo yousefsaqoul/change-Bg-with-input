2df69eb6cee53ab86d6d437ebe2594c366832de7
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-11 00:07:19 +0100

    change-Bg-with-input

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+//--------------------- --------------------------------
+const myColor = document.getElementById('my-color')
+
+function myButton() {
+     let farbe = myColor.value;
+     document.getElementById('replaceColor').style.background = farbe;
+}
+
+
+
+
